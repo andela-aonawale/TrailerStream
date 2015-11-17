1b2541ff6d2f93e5a99cc2e897248f308602241e
Ahmed Onawale <ahmed.onawale@andela.com>
2015-11-17 16:32:49 +0100

    Initial Commit

---
+++ TrailerStreamTests/TrailerStreamTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrailerStreamTests.swift
+//  TrailerStreamTests
+//
+//  Created by Ahmed Onawale on 11/17/15.
+//  Copyright © 2015 Ahmed Onawale. All rights reserved.
+//
+
+import XCTest
+@testable import TrailerStream
+
+class TrailerStreamTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
